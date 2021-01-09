3bede7c895972760dd2387ea716623f1f8a0f04b
Данила Кальнов <danyakalnov@gmail.com>
2021-01-09 10:47:30 +0300

    Add cors and body parser middlewares, sync sequelize

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,23 @@
 import * as koa from 'koa';
 import * as logger from 'koa-logger';
 import * as json from 'koa-json';
+import * as cors from 'koa__cors';
+import * as bodyParser from 'koa-bodyparser';
 import { taskRouter } from './routes/taskRoutes';
+import { sequelize } from './database/setup';
 
 const app = new koa();
 const PORT_NUMBER = 3000;
 
-app.use(json());
-app.use(logger());
-app.use(taskRouter.routes()).use(taskRouter.allowedMethods());
+(async () => {
+  await sequelize.sync();
+  app.use(cors());
+  app.use(json());
+  app.use(logger());
+  app.use(bodyParser());
+  app.use(taskRouter.routes()).use(taskRouter.allowedMethods());
 
-app.listen(PORT_NUMBER);
+  app.listen(PORT_NUMBER, () => {
+    console.log(`Koa server started at localhost:${PORT_NUMBER}`);
+  });
+})();
