f19340559f969842dd670e20c35243864b0932e3
Данила Кальнов <danyakalnov@gmail.com>
2021-01-09 11:32:22 +0300

    Add app-wide router with 'api' prefix

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import * as koa from 'koa';
 import * as logger from 'koa-logger';
 import * as json from 'koa-json';
 import * as cors from '@koa/cors';
+import * as koaRouter from 'koa-router';
 import * as bodyParser from 'koa-bodyparser';
 import { taskRouter } from './routes/taskRoutes';
 import { sequelize } from './database/setup';
@@ -9,13 +10,19 @@ import { sequelize } from './database/setup';
 const app = new koa();
 const PORT_NUMBER = 3000;
 
+const appRouter = new koaRouter({
+  prefix: '/api',
+});
+
+appRouter.use('/', taskRouter.routes(), taskRouter.allowedMethods());
+
 (async () => {
   await sequelize.sync();
   app.use(cors());
   app.use(json());
   app.use(logger());
   app.use(bodyParser());
-  app.use(taskRouter.routes()).use(taskRouter.allowedMethods());
+  app.use(appRouter.routes()).use(appRouter.allowedMethods());
 
   app.listen(PORT_NUMBER, () => {
     console.log(`Koa server started at localhost:${PORT_NUMBER}`);
